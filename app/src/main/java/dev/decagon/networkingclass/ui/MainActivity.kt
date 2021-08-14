422a5804f28f46adcc98ad044e6d26ac6b9d300e
Godday Okoduwa <goddayokos@gmail.com>
2021-08-14 15:42:17 +0100

    Added UI design and implemented the logic for adding new emojiPhrases

--- app/src/main/java/dev/decagon/networkingclass/ui/MainActivity.kt
+++ app/src/main/java/dev/decagon/networkingclass/ui/MainActivity.kt
@@ -5,19 +5,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import dev.decagon.networkingclass.App
 import dev.decagon.networkingclass.R
+import dev.decagon.networkingclass.model.request.EmojiPhraseRequest
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var swipeContainer: SwipeRefreshLayout
     private lateinit var fab: FloatingActionButton
+    private lateinit var progressBar: ProgressBar
     private lateinit var emptyListMsg: TextView
+    private lateinit var alertDialog: AlertDialog
 
     companion object {
         fun getIntent(context: Context): Intent {
@@ -26,6 +36,8 @@ class MainActivity : AppCompatActivity() {
             return intent
         }
     }
+
+    private val remoteApi = App.remoteApi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,6 +66,51 @@ class MainActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recycler_view)
         swipeContainer = findViewById(R.id.swipe_container)
         fab = findViewById(R.id.fab)
+        progressBar = findViewById(R.id.progress_bar)
         emptyListMsg = findViewById(R.id.empty_list_text)
+
+        fab.setOnClickListener { showAddEmojiPhrasesDialog() }
+    }
+
+    private fun showAddEmojiPhrasesDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_layout, null)
+        val emojiInput = dialogView.findViewById<TextInputEditText>(R.id.emoji)
+        val phraseInput = dialogView.findViewById<TextInputEditText>(R.id.phrase)
+
+        alertDialog = MaterialAlertDialogBuilder(this)
+            .setTitle("Add new emojiPhrase")
+            .setView(dialogView)
+            .setCancelable(false)
+            .setNegativeButton("CANCEL") { _, _ -> }
+            .setPositiveButton("DONE") { _, _ ->
+                val emoji = emojiInput.text.toString()
+                val phrase = phraseInput.text.toString()
+
+                if (emoji.isNotBlank() && phrase.isNotBlank()) {
+                    progressBar.visibility = View.VISIBLE
+                    val emojiPhrase = EmojiPhraseRequest(emoji, phrase)
+                    remoteApi.addEmojiPhrases(
+                        emojiPhrase,
+                        ::onError
+                    ) {
+                        progressBar.visibility = View.GONE
+                        Snackbar.make(
+                            swipeContainer,
+                            "New emojiPhrase added! by ${it.userId}",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+
+                } else {
+                    emojiInput.error = "This field must not be empty"
+                    phraseInput.error = "This field must not be empty"
+                }
+
+            }.show()
+    }
+
+    private fun onError(message: String) {
+        progressBar.visibility = View.GONE
+        Snackbar.make(swipeContainer, message, Snackbar.LENGTH_LONG).show()
     }
 }
